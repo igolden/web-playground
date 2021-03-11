d191737cba8f8e7df103778ab551fb80e8db89f1
Eli Richey <eli@elirichey.com>
2021-03-11 15:37:01 -0500

    form data serialized

--- file.js
+++ file.js
@@ -67,7 +67,7 @@ const ServusProfileManager = {
     });
   },
   submitWorkAreaForm: function (form) {
-    var copyData = decodeURIComponent(form);
+    let copyData = decodeURIComponent(form);
     let address_line_1 = copyData.split("address_line_1=").pop().split("&")[0];
     let address_line_2 = copyData.split("address_line_2=").pop().split("&")[0];
     let city = copyData.split("city=").pop().split("&")[0];
@@ -98,60 +98,111 @@ const ServusProfileManager = {
     });
   },
   submitProfileDetailsForm: function (form) {
-    let copyData = JSON.parse(form);
-    console.log("COPY: ", copyData);
+    let copyData = decodeURIComponent(form);
+    let email = copyData.split("email=").pop().split("&")[0];
+    let cell_phone = copyData.split("cell_phone=").pop().split("&")[0];
+    let cell_provider = copyData.split("cell_provider=").pop().split("&")[0];
+    let address_line_1 = copyData.split("address_line_1=").pop().split("&")[0];
+    let address_line_2 = copyData.split("address_line_2=").pop().split("&")[0];
+    let city = copyData.split("city=").pop().split("&")[0];
+    let state = copyData.split("state=").pop().split("&")[0];
+    let zip_code = copyData.split("zip_code=").pop().split("&")[0];
+    let country = copyData.split("country=").pop().split("&")[0];
+    let about = copyData.split("about=").pop().split("&")[0];
+
+    let serialized = {
+      email,
+      cell_phone,
+      cell_provider,
+      address_line_1,
+      address_line_2,
+      city,
+      state,
+      zip_code,
+      country,
+      about,
+    };
+
+    console.log("NEW_PROFILE_DETAILS!: ", serialized);
 
     $.ajax({
       url: "https://howdyeli.free.beeceptor.com/profile-details",
       type: "post",
-      data: form,
+      data: serialized,
       success: function () {
-        console.log("Submission Successful: ", form);
+        console.log("Submission Successful: ", serialized);
       },
     });
   },
   submitTaxForm: function (form) {
-    let copyData = JSON.parse(form);
-    console.log("COPY: ", copyData);
+    let copyData = decodeURIComponent(form);
+    let tax = copyData.split("tax=").pop().split("&")[0];
+
+    let serialized = {
+      tax,
+    };
+
+    console.log("NEW_WORK_AREA!: ", serialized);
 
     $.ajax({
       url: "https://howdyeli.free.beeceptor.com/tax",
       type: "post",
-      data: form,
+      data: serialized,
       success: function () {
-        console.log("Submission Successful: ", form);
+        console.log("Submission Successful: ", serialized);
       },
     });
   },
   submitPaymentForm: function (form) {
-    let copyData = JSON.parse(form);
-    console.log("COPY: ", copyData);
+    let copyData = decodeURIComponent(form);
+    let payment = copyData.split("payment=").pop().split("&")[0];
+
+    let serialized = {
+      payment,
+    };
+
+    console.log("NEW_WORK_AREA!: ", serialized);
 
     $.ajax({
       url: "https://howdyeli.free.beeceptor.com/payment",
       type: "post",
-      data: form,
+      data: serialized,
       success: function () {
-        console.log("Submission Successful: ", form);
+        console.log("Submission Successful: ", serialized);
       },
     });
   },
   submitInsuranceForm: function (form) {
-    let copyData = JSON.parse(form);
-    console.log("COPY: ", copyData);
+    let copyData = decodeURIComponent(form);
+    let insurance = copyData.split("insurance=").pop().split("&")[0];
+
+    let serialized = {
+      insurance,
+    };
+
+    console.log("NEW_WORK_AREA!: ", serialized);
 
     $.ajax({
       url: "https://howdyeli.free.beeceptor.com/insurance",
       type: "post",
-      data: form,
+      data: serialized,
       success: function () {
-        console.log("Submission Successful: ", form);
+        console.log("Submission Successful: ", serialized);
       },
     });
   },
   submitAgreementsForm: function (form) {
-    let copyData = JSON.parse(form);
-    console.log("COPY: ", copyData);
+    let copyData = decodeURIComponent(form);
+    let agreed_to_terms = copyData
+      .split("agreed_to_terms=")
+      .pop()
+      .split("&")[0];
+
+    let serialized = {
+      agreed_to_terms: agreed_to_terms === `on"` ? true : false,
+    };
+
+    console.log("NEW_WORK_AREA!: ", serialized);
 
     $.ajax({
       url: "https://howdyeli.free.beeceptor.com/agreement",
