14ce032a31ec44511a139886f6973b6d39de369d
Eli Richey <eli@elirichey.com>
2021-03-13 23:12:56 -0500

    added expires val to cookie

--- auth.js
+++ auth.js
@@ -35,7 +35,13 @@ const AuthManager = {
     return "";
   },
   setCookie: function (name, value) {
-    document.cookie = `${name}=${value}`;
+    var now = new Date();
+    var time = now.getTime();
+    time += 3600 * 1000;
+    now.setTime(time);
+    now.toUTCString();
+
+    document.cookie = `${name}=${value}; expires=${now.toUTCString()}`;
   },
   redirectToAuth: function () {
     window.location = "/?error=Session Expired. Please login again.";
